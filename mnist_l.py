3114f78917b8dfd5d70c87c425cbeb472f2276d6
jiangsongyi0204 <jiangsongyi0204@gmail.com>
2018-09-24 15:30:38 +0900

    Change img show position

--- mnist_l.py
+++ mnist_l.py
@@ -11,16 +11,28 @@ sensor = Sensor('EdgeSensor', 784)
 fc = FeatureColumn('FC',sensor,False)
 fc.importFmc('FC_v20180924_041816.txt')
 
+win1 = "Image"
+cv2.namedWindow(win1)
+cv2.moveWindow(win1, 40,30)
+
+win2 = "Features"
+cv2.namedWindow(win2)
+cv2.moveWindow(win2, 180,30)
+
+win3 = "Output"
+cv2.namedWindow(win3)
+cv2.moveWindow(win3, 500,30)
+
 for idx,row in enumerate(reader):
     x = np.array(row[1:])
     y = x.astype(np.float)
     image = np.reshape(y,(28,28))
     sensor.readMnist(image)
-    cv2.imshow("Sensor Image", cv2.resize(sensor.getSensorImg(),(100,100)))
+    cv2.imshow(win1, cv2.resize(sensor.getSensorImg(),(100,100)))
     fc.run()
-    cv2.imshow('Features',fc.getFeaturesImg())
-    if (row[0] == '1'):
-        cv2.imshow('FM',fc.getFeatureMapImg())
+    cv2.imshow(win2,fc.getFeaturesImg())
+    #if (row[0] == '1'):
+    cv2.imshow(win3,fc.getFeatureMapImg())
     if cv2.waitKey(33) >= 0:
         break
 
